f8ef58a2e8b4c057fcf2a518915538f15b679f01
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-06-02 16:16:24 +0600

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
+import Banner from './componant/Banner/Banner'
 // import Banner from './componant/Banner/Banner'
 import Footer from './componant/Footer/Footer'
+import Navbar from './componant/Navbar/Navbar'
+import TopHeader from './componant/TopHeader/TopHeader'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <div>
+      <TopHeader></TopHeader>
+      <Navbar></Navbar>
+      <Banner/>
       <Footer></Footer>
     </div>
   )
