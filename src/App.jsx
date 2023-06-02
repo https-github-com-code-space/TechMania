1c667b8bd2524bc98eba961402508dc0f055ffe5
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-06-02 14:20:15 +0600

    create app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Footer from './componant/Footer/Footer'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default App
